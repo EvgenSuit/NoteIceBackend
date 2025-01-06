22656ac5686634244ee07a693fead182c72e1b97
Evgeniy Mokich <mokichzhenya@gmail.com>
2025-01-06 16:41:13 +0100

    first commit

---
+++ src/main/java/com/example/noteice/NoteIceApplication.java
@@ -0,0 +1,13 @@
+package com.example.noteice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteIceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoteIceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/noteice/NoteIceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.noteice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteIceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
